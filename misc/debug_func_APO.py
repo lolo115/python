c5d8d7e94189fcd6ce1d6edaff3d782007b5cd3a
lolo115 <leturgezl@gmail.com>
2019-06-03 15:51:11 +0200

    commit

--- misc/debug_func_APO.py
+++ misc/debug_func_APO.py
@@ -23,9 +23,8 @@ def time_function(function):
 
     return wrapped
 
-
-@log_function
-@time_function
+# @log_function
+# @time_function
 def hello_fn(name='No one'):
     time.sleep(1)
     print('Hello "{}"'.format(name))
@@ -36,3 +35,5 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 # Execute the function
 hello_fn('Test')
+
+hello_fn('Test2')
