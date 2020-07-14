1486b21c207dbb19087b962a9dd63f7863ab3c24
laurent <laurent.leturgez@premiseo.com>
2020-07-14 12:39:11 +0200

    20200714

--- sandbox.py
+++ sandbox.py
@@ -1,2 +1,16 @@
-if __name__=='__main__':
-    print("coucou")
+from paramiko import SSHClient
+from scp import SCPClient
+import os
+
+with open("text.txt",'w+') as f:
+    f.write("coucou Pierrot \n")
+
+ssh = SSHClient()
+ssh.load_system_host_keys()
+ssh.connect(hostname='192.168.99.100', username='laurent', password='laurent')
+
+scp = SCPClient(ssh.get_transport())
+scp.put(files=["text.txt"],remote_path='/home/laurent/test',preserve_times=True)
+scp.close()
+
+os.remove("text.txt")
